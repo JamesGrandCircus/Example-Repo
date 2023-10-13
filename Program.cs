4163936f379d46e93fd527b98275cd7d79444ea0
Jakyle <jjackson360@gmail.com>
2023-10-12 21:45:50 -0400

    I'm finished with my lab, hooray

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Example3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // hello world
+            // hello world
+            // a final hello to you sir!... or maam
+        }
+    }
+}
